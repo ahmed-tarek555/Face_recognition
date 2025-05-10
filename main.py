e4c06e6a8fe827d0ff73c57909f68b719fbbb312
Ahmed <ahmedfather555@gmail.com>
2025-05-10 20:32:47 +0300

    just created the file ig

---
+++ main.py
@@ -0,0 +1,20 @@
+import torch
+
+def load_data(data_dir):
+    data = []
+    labels = []
+    classes = sorted(os.listdir(data_dir))
+    for idx, class_name in enumerate(classes):
+        class_path = os.path.join(data_dir, class_name)
+        if os.path.isdir(class_path):
+            for image_file in os.listdir(class_path):
+                image_path = os.path.join(class_path, image_file)
+                image = load_and_preprocess(image_path)
+                data.append(image)
+                labels.append(idx)
+
+    labels = torch.tensor(labels)
+    data = torch.tensor(np.array(data), dtype=torch.float)
+    return data, labels, classes
+
+
