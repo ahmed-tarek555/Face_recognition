77ef9a53feeae6cdc44e4016e0bbca4973e29e76
Ahmed <ahmedfather555@gmail.com>
2025-06-21 10:49:46 +0300

    created a function for loading the dataset

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import torch
+import os
+import numpy as np
+from PIL import Image
 
-def load_data(data_dir):
-    data = []
+
+target_size = (128, 128)
+target_format = 'L'
+
+
+def load_dataset(dataset_path, target_size):
+    x = []
+    y = []
     labels = []
-    classes = sorted(os.listdir(data_dir))
-    for idx, class_name in enumerate(classes):
-        class_path = os.path.join(data_dir, class_name)
-        if os.path.isdir(class_path):
-            for image_file in os.listdir(class_path):
-                image_path = os.path.join(class_path, image_file)
-                image = load_and_preprocess(image_path)
-                data.append(image)
-                labels.append(idx)
+    current_label = 0
 
-    labels = torch.tensor(labels)
-    data = torch.tensor(np.array(data), dtype=torch.float)
-    return data, labels, classes
+    for person_name in sorted(os.listdir(dataset_path)):
+        person_dir = os.path.join(dataset_path, person_name)
+        if not os.path.isdir(person_dir):
+            continue
 
+        labels.append(person_name)
 
+        for filename in os.listdir(person_dir):
+            file_path = os.path.join(person_dir, filename)
+
+            img = Image.open(file_path).convert(target_format)
+            img = img.resize(target_size)
+            img = np.array(img)/255.0
+
+            x.append(img)
+            y.append(current_label)
+
+        current_label += 1
+
+    x = torch.tensor(np.array(x), dtype=torch.float32)
+    y = torch.tensor(y)
+    return x, y, labels
+
+x, y, labels = load_dataset('data_set', target_size)
+
+print(y)
