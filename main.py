fbbb41b2872309d7c27f559e2df4efe19f99b066
Ahmed <ahmedfather555@gmail.com>
2025-06-22 11:20:48 +0300

    created 3 layers and calculated probs and the loss (not numerically stable for now)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 
-target_size = (128, 128)
+target_size = (64, 64)
 target_format = 'L'
 
 
@@ -39,4 +39,44 @@ def load_dataset(dataset_path, target_size):
 
 x, y, labels = load_dataset('data_set', target_size)
 
-print(y)
+n_hidden = 200
+
+x = x.view(x.shape[0], -1)
+
+# layer 1
+w1 = torch.randn(64*64, n_hidden)
+b1 = torch.randn(n_hidden)
+
+# layer 2
+w2 = torch.randn(n_hidden, n_hidden//2)
+b2 = torch.randn(n_hidden//2)
+
+# layer 3
+w3 = torch.randn(n_hidden//2, len(labels))
+
+parameters = [w1]+[b1]+[w2]+[b2]+[w3]
+for p in parameters:
+    p.requires_grad = True
+
+# forward pass
+x = x @ w1 + b1
+x = x @ w2 + b2
+logits = x @ w3
+
+probs = logits / torch.sum(logits, 1, keepdim=True)
+
+loss = -probs[range(0, probs.shape[0]), y].log().mean()
+
+# backward pass
+for p in parameters:
+    p.grad = None
+loss.backward()
+
+# update
+lr = 0.1
+for p in parameters:
+    p.data -= lr * p.grad
+
+
+print(loss)
+
