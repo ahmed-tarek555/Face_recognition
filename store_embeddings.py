4ecbd165906d33348d3dccbcb5d6aebf748b7e65
Ahmed <ahmedfather555@gmail.com>
2025-07-05 06:19:49 +0300

    now the store embedding function gets the average embedding of each person pictures and saves them in know_embeddings

--- store_embeddings.py
+++ store_embeddings.py
@@ -27,28 +27,28 @@ def get_embedding(pic):
     img_embedding = img_embedding.squeeze(0)
     return F.normalize(img_embedding, dim=0)
 
-def store_embeddings(path, pic_index, save_dir="known_embeddings"):
+def store_embeddings(path, save_dir="known_embeddings"):
     m.eval()
     os.makedirs(save_dir, exist_ok=True)
 
     for person_name in sorted(os.listdir(path)):
+        embeddings = []
         person_dir = os.path.join(path, person_name)
         if not os.path.isdir(person_dir):
             continue
 
         filenames = sorted(os.listdir(person_dir))
-        filename = filenames[pic_index]
-        pic = os.path.join(person_dir, filename)
-
-        img_embedding = get_embedding(pic)
-        if img_embedding is None:
-            continue
-
+        for filename in filenames:
+            pic = os.path.join(person_dir, filename)
+            img_embedding = get_embedding(pic)
+            if img_embedding is None:
+                continue
+            embeddings.append(img_embedding)
+        ave_embedding = torch.stack(embeddings, dim=0).mean(dim=0, keepdim=False)
         person_embedding_dir = os.path.join(save_dir, person_name)
         os.makedirs(person_embedding_dir, exist_ok=True)
-
         embedding_file = os.path.join(person_embedding_dir, f"{person_name}.pt")
-        torch.save(img_embedding, embedding_file)
+        torch.save(ave_embedding, embedding_file)
         print('Done')
 
-# store_embeddings('test/register', 0)
+# store_embeddings('test/register')
