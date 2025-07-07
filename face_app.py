11fe50e1b2a0334a31f52ce46c95f3d3cbec2c8f
Ahmed <ahmedfather555@gmail.com>
2025-07-07 16:42:10 +0300

    created a camera registration button which turns on the camera and catches 5 frames on only frames that contains a face and averages them and normalize and takes a name and creates a folder containing the embedding of that name in known embeddings

--- face_app.py
+++ face_app.py
@@ -1,12 +1,20 @@
+import torch
+import torch.nn.functional as F
 import tkinter as tk
 from tkinter import filedialog, Label, Button
 import os
 import shutil
 import cv2
 from PIL import Image
-
+from facenet_pytorch import MTCNN
 from recognize import recognize
 from store_embeddings import store_embeddings
+from utils import get_embedding
+from model import Model
+
+m = Model(105)
+m.load_state_dict(torch.load('parameters.pth'))
+mtcnn = MTCNN(image_size=128)
 
 
 class FaceApp:
@@ -14,26 +22,47 @@ class FaceApp:
         self.master = master
         master.title("Face Recognition")
 
-        self.camera_recognize_button = Button(master, text='Camera recognition', command=self.camera_detection)
-        self.camera_recognize_button.pack()
+        self.cameras_label = Label(text='Camera registration/ recognition')
+        self.cameras_label.grid(row=0, column=0, padx=50, pady=50)
+
+        self.camera_source_label = Label(text='Enter the camera source')
+        self.camera_source_label.grid(row=1, column=0, padx=50, pady=0)
 
         self.camera_source_entry = tk.Entry(root)
-        self.camera_source_entry.pack()
+        self.camera_source_entry.grid(row=2, column=0, padx=50, pady=0)
 
-        self.recognize_button = Button(master, text="Recognize", command=self.recognize)
-        self.recognize_button.pack(pady=20)
+        self.camera_register_label = Label(text='Enter the name of the user')
+        self.camera_register_label.grid(row=3, column=0, padx=50, pady=(30,0))
 
-        self.add_user_button = Button(master, text="Add user", command=self.add_user)
-        self.add_user_button.pack(pady=10)
+        self.camera_register_entry = tk.Entry(root)
+        self.camera_register_entry.grid(row=4, column=0, padx=50, pady=(0,0))
+
+        self.camera_register_button = Button(master, text='Camera register', command=self.camera_register)
+        self.camera_register_button.grid(row=5, column=0, padx=50, pady=(0,10))
+
+        self.camera_recognize_button = Button(master, text='Camera recognition', command=self.camera_detection)
+        self.camera_recognize_button.grid(row=6, column=0, padx=50, pady=20)
+
+        self.manual_label = Label(text='Manual registration/ recognition')
+        self.manual_label.grid(row=0, column=30, padx=50, pady=50)
+
+        self.pic_entry_label = Label(text='Enter name of the user')
+        self.pic_entry_label.grid(row=1, column=30, padx=50, pady=0)
 
         self.pic_entry = tk.Entry(root)
-        self.pic_entry.pack()
+        self.pic_entry.grid(row=2, column=30, padx=50, pady=0)
+
+        self.add_user_button = Button(master, text="Add user", command=self.add_user)
+        self.add_user_button.grid(row=3, column=30, padx=50, pady=(0, 30))
 
         self.register_users_button = Button(master, text="Register users", command=self.register_users)
-        self.register_users_button.pack(pady=30)
+        self.register_users_button.grid(row=4, column=30, padx=50, pady=(0, 30))
+
+        self.recognize_button = Button(master, text="Recognize", command=self.recognize)
+        self.recognize_button.grid(row=5, column=30, padx=50, pady=0)
 
         self.result_label = Label(master, text="")
-        self.result_label.pack()
+        self.result_label.grid()
 
 
     def add_user(self):
@@ -85,8 +114,47 @@ class FaceApp:
         cap.release()
         cv2.destroyAllWindows()
 
+    def camera_register(self):
+        m.eval()
+        source = self.camera_source_entry.get()
+        cap = cv2.VideoCapture(f"{source}")
+        name = self.camera_register_entry.get()
+
+        frame_count = 0
+        process_every_n = 15
+        embeddings = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            if frame_count % process_every_n == 0:
+                rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                pic = Image.fromarray(rgb)
+
+                if mtcnn(pic) is not None:
+                    embeddings.append(get_embedding(pic, m))
+
+
+            if len(embeddings) == 5:
+                ave_embedding = torch.stack(embeddings, dim=0).mean(dim=0, keepdim=False)
+                ave_embedding = F.normalize(ave_embedding, dim=0)
+                person_embedding_dir = f'known_embeddings/{name}'
+                os.makedirs(person_embedding_dir, exist_ok=True)
+                embedding_file = os.path.join(person_embedding_dir, f"{name}.pt")
+                torch.save(ave_embedding, embedding_file)
+                print('Done')
+                break
+
+            cv2.imshow("Video Face Recognition", frame)
+            frame_count += 1
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("600x500")
 app = FaceApp(root)
 root.mainloop()
